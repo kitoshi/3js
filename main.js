01ec216967adefefc717988a7aea06ef1e9d8154
kitoshi <kitoshi.charlton@gmail.com>
2022-10-04 20:04:30 -0700

    donut orbit

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './style.css';
 import * as THREE from 'three';
 
+//to move with mouse
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -23,14 +26,36 @@ camera.position.setZ(30);
 //add object
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
+
+//basic needs no light
+/* const material = new THREE.MeshBasicMaterial({
   color: 0xff6347,
   wireframe: true
+});
+ */
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff6347
 });
 
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+//need light for std material
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+
+//ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+//light helper: shows position
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   //tell browser to repaint
@@ -38,7 +63,7 @@ function animate() {
 
   //animate here
   torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
+  torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
   renderer.render(scene, camera);
